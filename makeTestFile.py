39c7e4fbb4496c4e692b04a48ff1cf5533880352
Bill Campbell <bill@vmhome.celestial.com>
2021-04-12 17:25:12 -0700

    new file:   makeTestFile.py 	Create test files by appending a file to the alertTest.

---
+++ makeTestFile.py
@@ -0,0 +1,78 @@
+#!/csrel27b/bin/python
+
+# $Header: /vol/cscvs/python-Csys/csmain.py,v 1.4 2009/11/25 07:49:41 csoftmgr Exp $
+# $Date: 2009/11/25 07:49:41 $
+
+import os, os.path, sys, re
+try: import Csys #{
+except: #{
+	sys.path.extend([
+		os.path.join(sys.prefix, 'lib/python'),
+		os.path.join(sys.prefix, 'lib/python/site-packages'),
+	])
+	import Csys
+#}}
+
+__doc__ = '''Make test file for BBOalert
+
+usage: %s''' % Csys.Config.progname
+
+__doc__ += '''
+
+$Id: csmain.py,v 1.4 2009/11/25 07:49:41 csoftmgr Exp $
+'''
+
+__version__='$Revision: 1.4 $'[11:-2]
+
+def main(): #{
+	# Add program options to parser here
+
+	def setOptions(): #{
+		'''Set command line options'''
+		global __doc__
+
+		parser = Csys.getopts(__doc__)
+
+	#	parser.add_option('-d', '--directory',
+	#		action='store', type='string', dest='directory', default=None,
+	#		help='Change to directory before starting process',
+	#	)
+	#	parser.add_option('-r', '--restart',
+	#		action='store_true', dest='restart', default=False,
+	#		help='restart without raising mailbox flags intially',
+	#	)
+		return parser
+	#} setOptions
+
+	parser = setOptions()
+
+	(options, args) = parser.parse_args()
+
+	verbose = ''
+	if options.verbose: #{
+		verbose = '-v'
+		sys.stdout = sys.stderr
+	#}
+	Csys.getoptionsEnvironment(options)
+	toptext = open('alertTester.txt', 'rb').read()
+	body = open(args[0]).read()
+	reOpts = re.MULTILINE|re.DOTALL|re.IGNORECASE
+	commentPattern = re.compile(r'^#.*?$', reOpts)
+	bboAlertPattern = re.compile(r'^\s*BBOalert.*?\n', reOpts)
+	body = commentPattern.sub('', body)
+	cmdPattern = re.compile(r'^', reOpts)
+	body = bboAlertPattern.sub('', body)
+	body = re.sub(r'^\s*?\n', '', body, flags=reOpts)
+	body = cmdPattern.sub('T', body)
+	body = re.sub(r'^T\s*\n', '', body, flags=reOpts).rstrip('T')
+	print(	'BBOalert\n%s\nScript,onDataLoad\n'
+			'let tests = `\n%s`\n\n'
+			'ALERTTESTER.runTests(tests);\n'
+			'Script'
+		) % (toptext, body)
+
+#}
+if __name__ == '__main__': #{
+	main()
+#}
+# vim: noexpandtab sw=4 ts=4 wm=0
